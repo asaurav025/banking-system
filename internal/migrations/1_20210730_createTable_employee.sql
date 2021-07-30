ab91a5402f65550d912ca7d4aeb60d750bcc0e93
asaurav025 <asaurav025@gmail.com>
2021-07-30 16:39:28 +0530

    Adding migration to create employee table

---
+++ internal/migrations/1_20210730_createTable_employee.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+
+CREATE TABLE employee
+(
+    id          UUID PRIMARY KEY    NOT NULL,
+    name        VARCHAR(225)        NOT NULL,
+    email       VARCHAR(100)        NOT NULL,
+    type        VARCHAR(20)         NOT NULL,
+    created_on  TIMESTAMP           NOT NULL DEFAULT Now(),
+    updated_on  TIMESTAMP           NOT NULL DEFAULT Now(),
+    created_by  VARCHAR(100)        NOT NULL,
+    updated_by  VARCHAR(100)        NOT NULL
+);
+
+CREATE UNIQUE INDEX employee_idx_email ON employee(email);
+
+-- +goose Down
+DROP TABLE IF EXISTS employee;
