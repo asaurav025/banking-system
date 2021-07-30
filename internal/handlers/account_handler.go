002ac9d17cdd499ed23b98e2ca05ad6f8863ac20
asaurav025 <asaurav025@gmail.com>
2021-07-30 22:18:33 +0530

    Adding account handler

---
+++ internal/handlers/account_handler.go
@@ -0,0 +1,66 @@
+package handlers
+
+import (
+	"banking-system/internal/dto"
+	"banking-system/internal/interfaces/iservices"
+	"context"
+	"net/http"
+	"sync"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo"
+	log "github.com/sirupsen/logrus"
+)
+
+type AccountHandler struct {
+	IAccountService iservices.IAccountService
+}
+
+var AccountHandlerInstance *AccountHandler
+var AccountHandlerOnce sync.Once
+
+func NewAccountHandler(
+	IAccountService iservices.IAccountService,
+) *AccountHandler {
+	if AccountHandlerInstance == nil {
+		AccountHandlerOnce.Do(func() {
+			AccountHandlerInstance = &AccountHandler{
+				IAccountService: IAccountService,
+			}
+		})
+	}
+	return AccountHandlerInstance
+}
+
+func (handler *AccountHandler) CreateAccount(c echo.Context) error {
+	log.Info("Method: Create account")
+	ctx := context.WithValue(httpContext, USER_ID, "")
+	requestBody := new(dto.AccountRequestDto)
+	bindErr := c.Bind(requestBody)
+	if bindErr != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+	acc, err := handler.IAccountService.Create(ctx, requestBody)
+	if err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+	return c.JSON(http.StatusCreated, acc)
+}
+
+func (handler *AccountHandler) GetBalance(c echo.Context) error {
+	log.Info("Method: Get balance")
+	ctx := context.WithValue(httpContext, USER_ID, "")
+	accountId, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+	acc, err := handler.IAccountService.GetAccount(ctx, accountId)
+	if err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+	return c.JSON(http.StatusCreated, &struct {
+		Balance uint `json:"balance"`
+	}{
+		Balance: acc.Balance,
+	})
+}
