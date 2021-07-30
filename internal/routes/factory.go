cf33ba4f6d12b0e784db0f88673e566dc756ed17
asaurav025 <asaurav025@gmail.com>
2021-07-30 14:25:57 +0530

    Fixing issue ralted to db intialization

--- internal/routes/factory.go
+++ internal/routes/factory.go
@@ -1,18 +1,18 @@
 package routes
 
 import (
+	"sync"
+
+	"banking-system/internal/db"
 	"banking-system/internal/handlers"
 	"banking-system/internal/repositories"
 	"banking-system/internal/services"
-	"banking-system/pkg/db"
-	"sync"
 )
 
 type controller struct{}
 
 func (c *controller) InjectDepencies() *handlers.EmployeeHandler {
 	dbInstance := db.GetDB()
-
 	employeeRepo := repositories.NewEmployeeRepository(dbInstance)
 	employeeService := services.NewEmployeeService(employeeRepo)
 	employeeHandler := handlers.NewEmployeeHandler(employeeService)
