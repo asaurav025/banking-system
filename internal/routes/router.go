b9d21344eb3a0f319d14e16984a95e9d439236c9
asaurav025 <asaurav025@gmail.com>
2021-07-30 16:38:56 +0530

    Adding endpoints for adding and deleting employee

--- internal/routes/router.go
+++ internal/routes/router.go
@@ -12,16 +12,19 @@ type Route struct {
 
 func (r *Route) Init() {
 
+	employeeHandler := Factory().InjectDepencies()
+
 	applicationGroup := r.Router.Group("/banking-system")
 	{
 		applicationGroup.GET("/health", func(c echo.Context) error {
 			return c.JSON(http.StatusOK, "I am healthy")
 		})
 
-		// v1 := applicationGroup.Group("/v1")
-		// {
-
-		// }
+		v1 := applicationGroup.Group("/v1")
+		{
+			v1.POST("/employee/add", employeeHandler.AddEmployee)
+			v1.DELETE("/employee/:id", employeeHandler.DeleteEmployee)
+		}
 	}
 
 }
