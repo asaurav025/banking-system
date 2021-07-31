9966804c5e47139d4512d3e7dafa0dda948a02b6
asaurav025 <asaurav025@gmail.com>
2021-07-31 14:45:24 +0530

    Adding transaction handler in factory

--- internal/routes/factory.go
+++ internal/routes/factory.go
@@ -11,23 +11,26 @@ import (
 
 type controller struct{}
 
-func (c *controller) InjectDepencies() (*handlers.EmployeeHandler, *handlers.CustomerHandler, *handlers.AccountHandler) {
+func (c *controller) InjectDepencies() (*handlers.EmployeeHandler, *handlers.CustomerHandler, *handlers.AccountHandler, *handlers.TransactionHandler) {
 	dbInstance := db.GetDB()
 	employeeRepo := repositories.NewEmployeeRepository(dbInstance)
 	customerRepo := repositories.NewCustomerRepository(dbInstance)
 	kycDetailsRepo := repositories.NewKycDetailsRepository(dbInstance)
 	accountRepo := repositories.NewAccountRepository(dbInstance)
+	transactionRepo := repositories.NewTransactionRepository(dbInstance)
 
 	employeeService := services.NewEmployeeService(employeeRepo)
 	customerService := services.NewCustomerService(customerRepo, kycDetailsRepo, accountRepo)
 	kycDetailsService := services.NewKycDetialsService(kycDetailsRepo)
 	accountService := services.NewAccountService(accountRepo)
+	transactionService := services.NewTransactionService(transactionRepo, accountRepo)
 
 	employeeHandler := handlers.NewEmployeeHandler(employeeService)
 	customerHandler := handlers.NewCustomerHandler(customerService, kycDetailsService)
 	accountHandler := handlers.NewAccountHandler(accountService)
+	transactionHandler := handlers.NewTransactionHandler(transactionService)
 
-	return employeeHandler, customerHandler, accountHandler
+	return employeeHandler, customerHandler, accountHandler, transactionHandler
 }
 
 var controllerInstance *controller
