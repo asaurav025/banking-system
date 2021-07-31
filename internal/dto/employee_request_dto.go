25422642a15a75dc1b66fe73b084e1e09ee55da4
asaurav025 <asaurav025@gmail.com>
2021-07-31 16:20:50 +0530

    Adding password in employee creation request

--- internal/dto/employee_request_dto.go
+++ internal/dto/employee_request_dto.go
@@ -1,6 +1,7 @@
 package dto
 
 type EmployeeRequestDto struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
